aad44af3cb368682358b14d7bfbb9fe4fb5d2975
Romi Bar <romibar@170.250.143.26.hwccustomers.com>
2026-05-15 01:04:19 -0400

    added comments to college_api.py

--- college_api.py
+++ college_api.py
@@ -2,16 +2,20 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() #opens and reads .env file
 
+#storing the API key and address in variables
 API_KEY = os.getenv("COLLEGE_SCORECARD_API_KEY")
 BASE_URL = "https://api.data.gov/ed/collegescorecard/v1/schools"
+
+#defining a function that takes the raw location the user typed and grabs the city and state separately returns both values back
 def parse_location(location):
     parts = location.split(",")
     city = parts[0].strip()
     state = parts[1].strip()
     return city, state
 
+#defining main function. takes state as required, city optional.
 def get_colleges(state, city=None):
     params = {
         "api_key": API_KEY,
@@ -26,5 +30,3 @@ def get_colleges(state, city=None):
     data = response.json()
 
     return data["results"]
-result = get_colleges("FL")
-print(result)
